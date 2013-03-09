de240b60f2ae6836adb748829e4a99bc6146303d
Ed Knutson <knutsoned@gmail.com>
2013-03-09 12:39:17 -0600

    fixed m2crypto submodule path

--- python-src/main.py
+++ python-src/main.py
@@ -1,25 +1,28 @@
+'''
 import kivy
 kivy.require('1.0.9')
 from kivy.uix.floatlayout import FloatLayout
 from kivy.app import App
 from kivy.properties import ObjectProperty, StringProperty
+'''
 
 import os
 import M2Crypto
 
-def empty_callback ():
- return
-
-class Controller(FloatLayout):
+#class Controller(FloatLayout):
+class Controller():
     '''Create a controller that receives a custom widget from the kv lang file.
 
     Add an action to be called from the kv lang file.
     '''
+    
+    ''' or not
     label_wid = ObjectProperty()
     otherlabel_wid = ObjectProperty()
     info = StringProperty()
     moreinfo = StringProperty()
-
+    '''
+    
     # handle button press
     def do_action(self):
 		M2Crypto.Rand.rand_seed (os.urandom (1024))
@@ -33,17 +36,20 @@ class Controller(FloatLayout):
 		SignEVP = M2Crypto.EVP.load_key_string(combinedkey)
 		SignEVP.sign_init()
 		SignEVP.sign_update('signing string')
+		
+		'''
 		self.moreinfo = SignEVP.sign_final().encode('base64')
 		
 		self.label_wid.text = 'My label after button press'
 		self.otherlabel_wid.text = self.moreinfo
 		self.info = 'New info text'
-	    
+	    '''
 
-class ControllerApp(App):
+class ControllerApp():
     def build(self):
         return Controller(info='Hello world')
 
-
+'''
 if __name__ == '__main__':
     ControllerApp().run()
+'''
